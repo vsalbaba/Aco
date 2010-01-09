e390e9f02eec786a73cf3723f91cb2e2d0be2ae3
Vojtech Salbaba <darktatka@gmail.com>
2010-01-09 20:52:55 +0100

    pridani updatu feromonu, metody pro ziskani reseni a pro zahajeni vypoctu
    Signed-off-by: Vojtech Salbaba <darktatka@gmail.com>

--- lib/ant_system.rb
+++ lib/ant_system.rb
@@ -1,16 +1,15 @@
 class AntSystem
-  ALPHA = 1
-  BETA  = 2
-  attr_accessor :trails, :graph, :alpha, :beta
+  attr_accessor :trails, :graph, :alpha, :beta, :ends
   def initialize(graph)
     @root = graph.shift
     @ends = graph.shift
     @graph = graph
     @trails = initialize_trails
     @constructed_solution = {}
-    @alpha = 1
-    @beta = 0.9
-    @evaporation_rate = 0.1
+    @alpha = 2 # trail impact
+    @beta = 0.9 # heuristics impact
+    @evaporation_rate = 0.8
+    @quality_coefficient = 1.0
   end
 
   def probability(u, v, k = 0, tabu_list = [])
@@ -33,8 +32,51 @@ class AntSystem
     @graph.include?([u, v]) ? 1 : 0
   end
 
-  def pheromone_update iteration
+  def run!(total_iterations, ants_in_iteration)
+    total_iterations.times do |i|
+      iteration = construct_iteration(ants_in_iteration)
+      pheromone_update(iteration)
+    end
+    self
+  end
 
+  def solution
+    sol = [@root]
+    current = @root
+    until @ends.include? current do
+      # find trail with highest pheromone level from current
+      max = @trails.max_by {|key, value|
+       if key.first == current then
+         value
+       else
+         -1
+       end
+      }
+      current = max.first.last
+      sol << current
+    end
+    sol
+  end
+
+  #τιψ (t) = ρ τιψ (t − 1) + ∆τιψ
+  #ρ, 0 ≤ ρ ≤ 1, is a user-defined parameter called
+  #evaporation coefficient, and ∆τιψ represents the
+  #sum of the contributions of all
+  #ants that used move (ιψ) to construct their solution.
+  def pheromone_update iterations
+    sum = sum_iterations(iterations)
+    @trails.each do |key, value|
+      add = sum[key] || 0.0
+      @trails[key] = @evaporation_rate * value + add
+    end
+  end
+
+  def construct_iteration(n)
+    result = []
+    n.times do
+      result << construct_solution
+    end
+    result
   end
 
   # will construct one solution by running an ant and return the ant path.
@@ -47,6 +89,7 @@ class AntSystem
     end
     path
   end
+
 
   # will pick next step with weighted random from position, ignoring tabuized items in tabu_list
   def next_step position, tabu_list
@@ -63,20 +106,34 @@ class AntSystem
   end
 
 private
+
+  def sum_iterations(iterations)
+    sum = {}
+    # vytvorit sumu vsech cest
+    iterations.each do |iteration|
+      iteration.inject do |first, last|
+        sum[[first, last]] ||= 0.0
+        sum[[first, last]] += path_quality(iteration)
+        last
+      end
+    end
+    sum
+  end
+
   def get_all_paths_from u, tabu_list = []
     @graph.find_all do |path|
       path.first == u and not tabu_list.include? path.last
     end
   end
-
-  def update_pheromone_for u, v, iteration
-    @trails[[u, v]] = evaporation_rate*trails[[u,v]]*(iteration-1) + @constructed_solution[[u,v]]
+  # Q / path.length
+  def path_quality(path)
+    @quality_coefficient / path.length
   end
 
   def initialize_trails
     @trails = {}
     @graph.each do |u, v|
-      @trails[[u, v]] = 0
+      @trails[[u, v]] = 0.0
     end
     @trails
   end
--- spec/ant_system_spec.rb
+++ spec/ant_system_spec.rb
@@ -1,6 +1,53 @@
 require  File.dirname(__FILE__) + '/spec_helper'
 
 describe AntSystem do
+
+  describe "solution" do
+    it 'should return some path for an untraveled graph' do
+      ending = 5
+      start = 1
+      graph = [start, [ending], [1, 2], [2, 3], [3, 4], [4, 5], [2, 4]]
+      ant_system = AntSystem.new(graph)
+      ant_system.solution.last.should == ending
+      ant_system.solution.first.should == start
+    end
+
+    it 'should return shortest path for an traveled graph' do
+      ending = 4
+      start = 1
+      graph = [start, [ending], [1, 2], [2, 3], [3, 4], [2, 5], [5, 6], [6, 7], [7, 8], [8, 3]]
+      ant_system = AntSystem.new(graph)
+      ant_system.run!(20, 5)
+      ant_system.solution.should have(4).items
+    end
+
+    it 'should return shortest path for medium graph' do
+      ending = 11
+      start = 1
+      graph = [start,
+              [ending],
+              [1, 3], [1, 4],
+              [3, 2], [3, 5], [3, 6], [3, 13],
+              [4, 13], [4, 7],
+              [2, 5],
+              [5, 6], [5, 12],
+              [6, 8], [6, 9],
+              [13, 7], [13, 6],
+              [7, 9],
+              [12, 8],
+              [9, 8], [9, 10],
+              [8, 10], [8, 11],
+              [10, 11]]
+      ant_system = AntSystem.new(graph)
+      ant_system.run!(1, 6)
+      solution = ant_system.solution
+      puts ant_system.trails.inspect
+      puts solution
+      solution.should have(5).items
+    end
+  end
+
+
   describe "probability without pheromones" do
     before :each do
       @graph = [1, [5], [1, 2], [2, 3], [3, 4], [4, 5], [2, 4]]
@@ -22,10 +69,11 @@ describe AntSystem do
 
   describe "probability with pheromones" do
     before :each do
-        @graph = [1, [5], [1, 2], [2, 3], [3, 4], [4, 5], [2, 6], [6, 4]]
-        @ant_system = AntSystem.new(@graph)
-        @ant_system.trails[[2, 6]] += 1
-      end
+      @graph = [1, [5], [1, 2], [2, 3], [3, 4], [4, 5], [2, 6], [6, 4]]
+      @ant_system = AntSystem.new(@graph)
+      @ant_system.trails[[2, 6]] += 1
+    end
+
     it "pheromoned trails should have higher probability" do
       unferomoned = @ant_system.probability(2, 3, 1)
       feromoned = @ant_system.probability(2, 6, 1)
@@ -39,7 +87,74 @@ describe AntSystem do
     end
   end
 
-  describe "pheromone update" do
+  describe 'pheromone_update' do
+    before :each do
+      @graph = [1, [5], [1, 2], [2, 3], [3, 4], [4, 5], [2, 6], [6, 4]]
+      @ant_system = AntSystem.new(@graph)
+    end
+
+    describe 'sum_iterations' do
+      it 'sum of 2 equal paths should be double of 1 path' do
+        iteration = @ant_system.send(:sum_iterations, [[1,2,3,4,5]])
+        iteration2 = @ant_system.send(:sum_iterations, [[1,2,3,4,5], [1,2,3,4,5]])
+        iteration2.each do |key, value|
+          value.should == (2*iteration[key])
+        end
+      end
+
+      it 'should assign high value on paths which were used more and low value on paths which were used less' do
+        iteration = @ant_system.send(:sum_iterations, [[1,2,3,4,5], [1,2,3]])
+        iteration[[1,2]].should > iteration[[3,4]]
+      end
+
+      it 'should sum all paths which were used in iteration if none paths are intersecting' do
+        iteration = @ant_system.send(:sum_iterations, [[1,2], [2,3], [3, 4], [4, 5]])
+        iteration.should have_key([1,2])
+        iteration.should have_key([2,3])
+        iteration.should have_key([3,4])
+        iteration.should have_key([4,5])
+      end
+
+      it 'should sum all paths which were used in iteration even if paths are intersecting' do
+        iteration = @ant_system.send(:sum_iterations, [[1,2,3], [2,3,4], [3, 4, 5], [4, 5]])
+        iteration.should have_key([1,2])
+        iteration.should have_key([2,3])
+        iteration.should have_key([3,4])
+        iteration.should have_key([4,5])
+      end
+
+      it 'should handle iterations 1,2,3,4,5], [1,2,3]' do
+        iteration = @ant_system.send(:sum_iterations, [[1,2,3,4,5], [1,2,3]])
+        iteration.should have_key([1,2])
+        iteration.should have_key([2,3])
+        iteration.should have_key([3,4])
+        iteration.should have_key([4,5])
+      end
+    end
+
+    it 'should change path value if no ant run over it' do
+      before = @ant_system.trails[[1,2]]=(1.0)
+      @ant_system.pheromone_update([])
+      @ant_system.trails[[1,2]].should < before
+    end
+
+    it 'should increase the value of path if it was run over by sufficient ants' do
+      before = @ant_system.trails[[1,2]]
+      @ant_system.pheromone_update([[1, 2, 3, 4, 5], [1,2,3,4,5], [1,2,3,4,5]])
+      @ant_system.trails[[1,2]].should > before
+    end
+  end
+
+  describe "path_quality" do
+    before :each do
+      @path = Array.new(10)
+      @graph = [1, [5], [1, 2], [2, 3], [3, 4], [4, 5], [2, 6], [6, 4]]
+      @ant_system = AntSystem.new(@graph)
+    end
+
+    it 'should return  1/10 if if path length is 10 and evaporation_rate is 1.0' do
+      @ant_system.send(:path_quality, @path).should == 1.0/10
+    end
   end
 
   describe "next_step" do
@@ -74,6 +189,20 @@ describe AntSystem do
     end
   end
 
+
+  describe "run iteration" do
+    before :each do
+      @graph = [1, [5], [1, 2], [2, 3], [3, 4], [4, 5], [2, 6], [6, 4]]
+      @ant_system = AntSystem.new(@graph)
+      @ant_system.trails[[2, 6]] += 1
+    end
+
+    it 'should depend on construct_solution' do
+      @ant_system.should_receive(:construct_solution).exactly(20).times
+      @ant_system.construct_iteration(20)
+    end
+  end
+
   describe "construct_solution" do
     before :each do
       @graph = [1, [5], [1, 2], [2, 3], [3, 4], [4, 5], [2, 6], [6, 4]]
@@ -87,7 +216,7 @@ describe AntSystem do
 
     it 'should return route through node 6 more times' do
       count = {}
-      3000.times do
+      100.times do
         solution = @ant_system.construct_solution
         if solution.include?(6) then
           count[6] ||= 0
@@ -100,9 +229,9 @@ describe AntSystem do
       count[6].should > count[3]
     end
 
-    it 'should still use the route through node 3 sometimes' do
+    it 'should still use the route through node 3 sometimes ' do
       count = {}
-      3000.times do
+      100.times do
         solution = @ant_system.construct_solution
         if solution.include?(3) then
           count[3] ||= 0
